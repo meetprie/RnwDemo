44b46aa50e295e66892581d4b4463d1cece5ba66
meetprie <55573052+meetprie@users.noreply.github.com>
2020-07-23 18:23:27 +0530

    Kisan Tranding Uplode

---
+++ js/main.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+
+	$(document).on('click', '.nav-link', function(e) {
+	    var id = $(this).attr('href');
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+	    e.preventDefault();
+	    var pos = $id.offset().top;
+	    $('body, html').animate({scrollTop: pos},500);
+	});
+	$(window).scroll(function() {
+		var header = $('.header_bottom');
+		var scroll = $(window).scrollTop();
+
+		if (scroll > 300) {
+			header.addClass('fixed');
+		}
+		else{
+			header.removeClass('fixed');
+		}
+	});
+	$('.testimonials_slider').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:false,
+	    dots:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
+	$('.client_logo_slider').owlCarousel({
+			loop:false,
+			margin:20,
+			dots:true,
+			nav:true,
+			autoplay:true,
+			autoplayTimeout:2000,
+			autoplayHoverPause:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				400:{
+					items:2
+				},
+				600:{
+					items:3
+				},
+				900:{
+					items:4
+				},
+				1000:{
+					items:5
+				},
+				1200:{
+					items:6
+				}
+			}
+		});
+
+
+	$('.success_storics_slider').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    // dots: false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1,
+	            dots: true,
+	        },
+	        1000:{
+	            items:3
+	        },
+	        1200:{
+	        	items: 3
+	        }
+	    }
+	});
+	$(window).scroll(function(){ 
+       if ($(this).scrollTop() > 100) { 
+           $('.scrollToTop').fadeIn(); 
+       } else { 
+           $('.scrollToTop').fadeOut(); 
+       } 
+   }); 
+   $('.scrollToTop').click(function(){ 
+       $("html, body").animate({ scrollTop: 0 }, 600); 
+       return false; 
+   });
+});
